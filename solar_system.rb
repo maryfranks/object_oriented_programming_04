eddea26771048777948a5b1bb6e9e8c63342943d
maryfranks <maryfranks@gmail.com>
2017-07-07 15:10:08 -0400

    makes a solar system

---
+++ solar_system.rb
@@ -0,0 +1,70 @@
+class System
+
+
+  @@bodies = []
+
+  def self.all
+    @@bodies
+  end
+
+  def add
+    @@bodies << Body.new
+    return @@bodies.last
+  end
+
+  def total_mass
+    # add up all the masses of all the bodies
+    sum = 0
+    @@bodies.each do |body|
+      sum += body.mass
+    end
+    return sum
+  end
+
+end
+
+class Body
+
+  attr_reader :mass, :name
+
+  def initialize(name, mass)
+    @name = name
+    @mass = mass
+  end
+
+end
+
+class Star < Body
+
+attr_reader :type
+
+  def initialize(name, mass, type)
+    super(name, mass)
+    @type = type
+  end
+
+end
+
+class Planet < Body
+
+  attr_reader :day, :year
+
+  def initialize(name, mass, hours_in_a_day, days_in_year)
+    super(name, mass)
+    @day = hours_in_a_day
+    @year = days_in_year
+  end
+
+end
+
+class Moon < Body
+
+  attr_reader :month, :planet
+
+  def initialize(name, mass, days_to_orbit_planet, planet_it_orbits)
+    super(name, mass)
+    @month = days_to_orbit_planet
+    @planet = planet_it_orbits
+  end
+
+end
